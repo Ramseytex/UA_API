2f7340412a896fbda4a2e3ddde5c1a08af9ffb6b
Ramsey Kail <Ramsey.Kail@urbanairship.com>
2013-06-10 14:28:06 -0700

    fixed issue with 405 error. *now it works*

--- Ruby/blast.rb
+++ Ruby/blast.rb
@@ -1,28 +1,15 @@
-require 'net/http'
-
 require 'rubygems'
+require 'httparty'
+require 'highline/import'
+require 'pp'
 require 'json'
 
-@user = 'Xy0gYfzLQfKYSvZuBW7R-Q'
-@pass = 'ISPsiGzqSEWa2lW48ox9og'
-@host = 'go.urbanairship.com'
-@port = '443'
-
-@post_ws = "/api/push/"
-
-@payload = {"apids"=> ["0748a54b-faf7-48eb-9db6-051da3ec5093"], "android"=> {"alert"=> "Hello!"}}.to_json
-  
-def post
-     req = Net::HTTP::Post.new(@post_ws, initheader = {'Content-Type' =>'application/json'})
-     	req.use_ssl = true
-		req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          req.basic_auth @user, @pass
-          req.body = @payload
-          response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
-           puts "Response #{response.code} #{response.message}:
-          #{response.body}"
-        end
-
-thepost = post
-
-puts thepost
+class Apipush
+	  include HTTParty
+	    base_uri "https://go.urbanairship.com"
+		basic_auth 'Xy0gYfzLQfKYSvZuBW7R-Q', 'ISPsiGzqSEWa2lW48ox9og'
+	    default_params 
+  		format :json
+  		end
+  	#"apids"=> ["0748a54b-faf7-48eb-9db6-051da3ec5093"]
+pp Apipush.post('/api/push/broadcast/', :body => {"android"=> {"alert"=> "Hello!"}}.to_json, :headers => {'Content-type' => 'application/json'}).inspect
