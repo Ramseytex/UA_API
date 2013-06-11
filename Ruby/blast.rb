d6b6ae556cc2f30e11f225b51c69f181c12cdff7
Ramsey Kail <Ramsey.Kail@urbanairship.com>
2013-06-11 08:05:12 -0700

    added aps and android to the broadcast

--- Ruby/blast.rb
+++ Ruby/blast.rb
@@ -12,4 +12,4 @@ class Apipush
   		format :json
   		end
   	#"apids"=> ["0748a54b-faf7-48eb-9db6-051da3ec5093"]
-pp Apipush.post('/api/push/broadcast/', :body => {"android"=> {"alert"=> "Hello!"}}.to_json, :headers => {'Content-type' => 'application/json'}).inspect
+pp Apipush.post('/api/push/broadcast/', :body => {"aps"=> {"alert"=> "Hello!"},"android"=> {"alert"=> "Hello!"}}.to_json, :headers => {'Content-type' => 'application/json'}).inspect
