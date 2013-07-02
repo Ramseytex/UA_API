9f5e6ec50cb45e46e8113b3a98c53dc691be02dd
Ramsey Kail <Ramsey.Kail@urbanairship.com>
2013-07-02 15:57:16 -0700

    blast.rb

--- Ruby/blast.rb
+++ Ruby/blast.rb
@@ -8,11 +8,11 @@ class Apipush
 	  include HTTParty
 	    base_uri "https://go.urbanairship.com"
 		# Push Sample
-		basic_auth 'Xy0gYfzLQfKYSvZuBW7R-Q', 'ISPsiGzqSEWa2lW48ox9og'
+		#basic_auth 'Xy0gYfzLQfKYSvZuBW7R-Q', 'ISPsiGzqSEWa2lW48ox9og'
 		#Rich Push
 		#basic_auth '_MDBRjmfTLCaLtRrRoZWmg', 'K7AgJinNT7yX0vkFonxwHA'
 		#Goat
-		#basic_auth 'ISex_TTJRuarzs9-o_Gkhg', '8kw22E_uTHaH6KL5Wiuk0g'
+		basic_auth 'ISex_TTJRuarzs9-o_Gkhg', '8kw22E_uTHaH6KL5Wiuk0g'
 	    default_params 
   		#format :json
   		end
@@ -23,24 +23,48 @@ class Apipush
 #pp Apipush.post('/api/push/', :body => {"audience" => "all", "device_types" => "all", "notification" => {"alert" => "testing 123"}}.to_json, :headers => {"Content-type" => "application/json", "Accept" => "application/vnd.urbanairship+json; version=3;"}).inspect
 
 #API v3 Goat Push
-pp Apipush.post('/api/push/', :body => {"audience" => "all", "device_types" => "all", "notification" => {"alert" => "testing123","android" => {"alert" => "push 2", "collapse_key"=> "gobbledygook", "delay_while_idle" => false}}}.to_json, :headers => {"Content-type" => "application/json", "Accept" => "application/vnd.urbanairship+json; version=3;"}).inspect
+#pp Apipush.post('/api/push/', :body => {"audience" => "all", "device_types" => "all", "notification" => {"alert" => "testing123","android" => {"alert" => "push 2", "collapse_key"=> "gobbledygook", "delay_while_idle" => false}}}.to_json, :headers => {"Content-type" => "application/json", "Accept" => "application/vnd.urbanairship+json; version=3;"}).inspect
 
+
+#API v3 Goat Rich
+=begin
+pp Apipush.post('/api/push/', 
+:body => 
+	{"audience" => 
+			{"tag"=> "surfing"}, 
+		"device_types" => ["android"], 
+			"notification" => 
+				{"alert" => "Goat API v3"}, "message"=> {
+        "title"=> "This week's offer",
+        "body"=> "<html><body><h1>blah blah</h1> etc...</html>",
+        "content_type"=> "text/html",
+        "extra"=> {
+            "offer_id" => "608f1f6c-8860-c617-a803-b187b491568e"
+        }
+    }}.to_json, 
+:headers => 
+	{'Content-type' => 'application/vnd.urbanairship+json; version=3;', 
+		'Accept' => 'application/vnd.urbanairship+json; version=3;'}).inspect
+=end
 #API v3 Goat schedule
 =begin
 pp Apipush.post('/api/schedules/', 
 :body => 
 	{"schedule" => 
-		{"local_scheduled_time" => "2013-06-13T14:15:00"},
+		{"scheduled_time" => "2013-07-2T22:25:00"},
 	"push" => 
 		{"audience" => 
-			{"location" => 
-				{"us_zip" => "97007",
-					"date" => 
-						{"recent" => 
-							{"days" => 4 }}}}, 
-		"device_types" => "all", 
+			{"tag"=> "surfing"}, 
+		"device_types" => ["android"], 
 			"notification" => 
-				{"alert" => "Goat API v3x"}}}.to_json, 
+				{"alert" => "Scheduled Rich Push"}, "message"=> {
+        "title"=> "Scheduled Rich Message",
+        "body"=> "<html><body><h1>Scheduled Rich Message yo!</h1>Awesome!</html>",
+        "content_type"=> "text/html",
+        "extra"=> {
+            "offer_id" => "608f1f6c-8860-c617-a803-b187b491568e"
+        }
+    }}}.to_json, 
 :headers => 
 	{'Content-type' => 'application/vnd.urbanairship+json; version=3;', 
 		'Accept' => 'application/vnd.urbanairship+json; version=3;'}).inspect
